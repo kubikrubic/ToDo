3e32a8c56b2d414da59e0f8febe3b320a115f6c9
kubikrubic <131974022+kubikrubic@users.noreply.github.com>
2023-08-21 01:17:42 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const button__add = document.querySelector('#add-button')
+const button__delete = document.querySelector('#delete')
+
+
+
+button__add.addEventListener('click', () => {
+	const input = document.querySelector('#input')
+	const inputValue = input.value
+	if (inputValue === '') {
+		return
+	} else {
+		const newIl = document.createElement('li')
+		newIl.textContent = inputValue 
+		newIl.classList.add('unchecked')
+		list.insertBefore(newIl, list.firstChild)
+		const span = document.createElement('span')
+		span.classList.add('cross')
+		span.textContent = '\u00d7'
+		newIl.appendChild(span)
+		input.value = ""
+	}
+})
+
+
+const list = document.querySelector('#list')
+
+list.addEventListener('click', e => {
+	if(e.target.tagName === 'SPAN') {
+		e.target.parentElement.remove()
+	} else if(e.target.tagName === 'LI') {
+		e.target.classList.toggle('checked')
+		}
+	}
+)
+
+button__delete.addEventListener('click', () => {
+	list.innerHTML = ''
+})
+
+
+
